0c34dd7b121b7ada8dca587e4e9c641a46b6e61d
mongo <mongopa88@gmail.com>
2019-03-16 23:59:34 +0900

    another answer

--- ABC081B/ABC081B.py
+++ ABC081B/ABC081B.py
@@ -1,12 +1,20 @@
 import sys
-s = int(input())
-li = [int(_) for _ in input().split()]
-c = 0
-while True:
-  for i in range(s):
-    if li[i] % 2 == 0:
-      li[i] //= 2
-    else:
-      print(c)
-      sys.exit()
-  c += 1
+# s = int(input())
+# li = [int(_) for _ in input().split()]
+# c = 0
+# while True:
+#   for i in range(s):
+#     if li[i] % 2 == 0:
+#       li[i] //= 2
+#     else:
+#       print(c)
+#       sys.exit()
+#   c += 1
+
+import math
+n = input()
+a = list(map(int, input().split()))
+ans = float("inf")
+for i in a:
+    ans = min(ans, len(bin(i)) - bin(i).rfind("1") - 1)
+print(round(ans))
