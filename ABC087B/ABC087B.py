b5a4bb78a1aa7ec210fbc424c9d17237f3dc5a19
mongo <mongopa88@gmail.com>
2019-03-17 22:41:14 +0900

    another answer

--- ABC087B/ABC087B.py
+++ ABC087B/ABC087B.py
@@ -1,12 +1,14 @@
-A = int(input())
-B = int(input())
-C = int(input())
-X = int(input())
+# A = int(input())
+# B = int(input())
+# C = int(input())
+# X = int(input())
+
+A, B, C, X = map(int, [input() for i in range(4)])
 
 cnt = 0
 for i in range(A + 1):
   for j in range(B + 1):
     for k in range(C + 1):
-      if (500 * i  + 100 * j + 50 * k) == X:
+      if 500 * i  + 100 * j + 50 * k == X:
         cnt += 1
 print(cnt)
