fc71768bb687ae3b05f0968b7282ee1e7dba6a2f
mongo <mongopa88@gmail.com>
2019-04-06 23:59:12 +0900

    ok

--- ABC090/b.py
+++ ABC090/b.py
@@ -2,7 +2,7 @@ ans = 0
 A, B  = map(int, input().split())
 
 for i in range(A, B+1):
-  if str(i)[0] == str(i)[-1] and str(i)[1] and str(i)[2]:
+  if str(i)[0] == str(i)[-1] and str(i)[1] == str(i)[-2]:
     ans += 1
 
 
